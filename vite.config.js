b86a09857ed589a765640b9a0ac386f661d0a2aa
unknown <karl.mullr@gmail.com>
2023-07-28 23:10:33 +0200

    bg ardoise 2

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css', 
                 'resources/js/app.js',
+                'resources/js/components/ModaleReservation.vue',
 
                 'resources/js/public/index.js',
                 'resources/js/public/modules/login-register.js',
