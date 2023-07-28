b68331fa2853c8109f6154d05b3bbd0f7e5e947b
unknown <karl.mullr@gmail.com>
2023-07-28 21:05:44 +0200

    changement sur le boot de formules

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\View\Components\carte\formules;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::component('carte.formules', formules::class);
     }
 }
