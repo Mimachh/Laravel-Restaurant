85fd493e17b3bf6697b2fd504c8cad8e75b94ba2
unknown <karl.mullr@gmail.com>
2023-07-29 10:11:48 +0200

    nveau format image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="Content-Security-Policy" content="connect-src 'self'; object-src 'self';
-        script-src 'self' https://unpkg.com/swiper/swiper-bundle.min.css https://cdn.jsdelivr.net/npm/axios@latest/dist/axios.min.js;
+        script-src 'self' https://unpkg.com/swiper/swiper-bundle.min.js https://cdn.jsdelivr.net/npm/axios@latest/dist/axios.min.js;
         ">
         <title>{{ env('APP_NAME')}}</title>
         <meta name="description" content="Votre restaurant en ligne.">
