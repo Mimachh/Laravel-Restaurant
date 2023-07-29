3547f7a101d777d1c9f3ea90c73f38d4ad3297fa
unknown <karl.mullr@gmail.com>
2023-07-29 09:59:39 +0200

    nveau format image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta http-equiv="Content-Security-Policy" content="connect-src 'self'">
+        <meta http-equiv="Content-Security-Policy" content="connect-src 'self'; object-src 'self';
+        script-src 'self' https://unpkg.com https://cdn.jsdelivr.net/npm/axios@latest/dist/axios.min.js;
+        ">
         <title>{{ env('APP_NAME')}}</title>
         <meta name="description" content="Votre restaurant en ligne.">
         <link
