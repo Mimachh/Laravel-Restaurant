d0d3da63f6f147c3f14aa6cc455952534d895239
unknown <karl.mullr@gmail.com>
2023-07-29 09:21:56 +0200

    nveau format image

--- resources/views/components/header/social.blade.php
+++ resources/views/components/header/social.blade.php
@@ -1,17 +1,23 @@
 <nav class="md:w-1/2 w-full mt-4 md:mt-0">
     <ul class="flex gap-3 justify-center md:justify-end">
         <li>
-            <a href="">
+            <a href="https://google.com"
+            aria-label="Lien de rs"
+            >
                 <x-jam-facebook class="social-icon text-light hover:text-primary transition-colors duration-100 ease"/>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="https://google.com"
+            aria-label="Lien de rs"
+            >
                 <x-jam-instagram  class="social-icon text-light hover:text-primary transition-colors duration-100 ease"/>
             </a>
         </li>
         <li>
-            <a href="">
+            <a href="https://google.com"
+            aria-label="Lien de rs"
+            >
                 <x-jam-twitter class="social-icon text-light hover:text-primary transition-colors duration-100 ease"/>
             </a>
         </li>
