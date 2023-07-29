94bce29f6a37c0d47c33bb93c3258689b84ffa0f
unknown <karl.mullr@gmail.com>
2023-07-29 09:20:08 +0200

    nveau format image

--- resources/views/components/header/contact.blade.php
+++ resources/views/components/header/contact.blade.php
@@ -1,10 +1,14 @@
 <div class="md:w-1/2 w-full flex gap-6 justify-center md:justify-start">
 <p>
         <!-- Lien pour le téléphone -->
-        <a href="tel:{{ env('TEL_ADMIN') }}" class="underline hover:text-primary transition-colors duration-100 ease">{{ env('TEL_ADMIN') }}</a>
+        <a href="tel:{{ env('TEL_ADMIN') }}" class="underline hover:text-primary transition-colors duration-100 ease"
+        aria-label="numéro du restaurant"
+        >{{ env('TEL_ADMIN') }}</a>
     </p>
     <p>
         <!-- Lien pour l'e-mail -->
-        <a href="mailto:{{ env('MAIL_ADMIN') }}" class="underline hover:text-primary transition-colors duration-100 ease">{{ env('MAIL_ADMIN') }}</a>
+        <a href="mailto:{{ env('MAIL_ADMIN') }}" class="underline hover:text-primary transition-colors duration-100 ease"
+        aria-label="adresse mail du restaurant"
+        >{{ env('MAIL_ADMIN') }}</a>
     </p>
 </div>
